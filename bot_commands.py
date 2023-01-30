ac2b8c97b712e629605c381877c9a25bc1b1b8d2
Pavel_Vatsek <pvatsek@gmail.com>
2023-01-30 23:12:22 +0300

    add python3 venv

---
+++ bot_commands.py
@@ -0,0 +1,12 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'Привет {update.effective_user.first_name}!')
+
+async def sum(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    msg = update.message.text
+    items = msg.split()
+    x = int(items[1])
+    y = int(items[2])
+    await update.message.reply_text(f'{x} + {y} = {x + y}')
--- main.py
+++ main.py
@@ -1,41 +1,12 @@
-from random import randrange
-import time
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from bot_commands import *
 
-def input_and_check_the_number_of_candies(message):
-    quantity = int(input(message))
-    while quantity > 28 or quantity < 1:
-        quantity = int(input('Можно взять от 1 до 28 конфет. Введите снова: '))
-    return quantity
+app = ApplicationBuilder().token("5918976065:AAGbKQWe8gC0q-yc9YPqqmLiyN4Mk15prYc").build()
 
-candys = 2021
-id = randrange(0,2)
+app.add_handler(CommandHandler("hello", hello))
+app.add_handler(CommandHandler("sum", sum))
 
-print('На столе лежит 2021 конфета. Два игрока делают ход друг после друга. Первый ход определяется жеребьёвкой.\
-За один ход можно забрать не более чем 28 конфет. Все конфеты оппонента достаются сделавшему последний ход. ')
-time.sleep(1)
-print(f'Всего на столе {candys} конфет')
-if id == '0':
-    print('Игру начинаете Вы')
-else: print('Игру начинает компьютер')
-time.sleep(1)
-while(candys > 0):
+print('Server Start')
 
-    match id:
-        case '0':
-            quantity = input_and_check_the_number_of_candies('Введите какое количество конфет хотите взять: ')
-            candys -= quantity
-            print(f'На столе осталось {candys} конфет')
-            id = '1'
-        case _:
-            if candys < 29:
-                quantity = candys
-            else:
-                quantity = randrange(1,29)
-            print(f'Компьютер берет {quantity} конфет')
-            candys -= quantity
-            print(f'На столе осталось {candys} конфет')
-            id = '0'
-if id == '1':
-    print('Ты выиграл))!!')
-else: print('Ты проиграл :((')
-
+app.run_polling()
